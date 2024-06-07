6b07f7a1578d6d1825b2cf962912ec9c4a297f0c
irisChenyi <chenyi4as@163.com>
2024-06-07 21:48:07 +0800

    add db connect

--- douban/index.py
+++ douban/index.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from time import sleep
 from bs4 import BeautifulSoup
+import mysql.connector
 import re
 import os
 
@@ -10,6 +11,9 @@ class getFile:
         self.url = url
 
     def getHtml(self):
+        self.mydb = []
+        self.name = self.content = self.replaysContent = self.time = ''
+
         option = webdriver.ChromeOptions()
         option.add_experimental_option("detach", True)
         self.driver = driver = webdriver.Chrome(options = option)
@@ -17,7 +21,9 @@ class getFile:
         driver.maximize_window()
         source = self.driver.page_source
         self.getContent(source)
-        self.driver.quit()
+        # self.driver.quit()
+        # 
+        return self
     
 
     def getContent(self, content):
@@ -30,15 +36,56 @@ class getFile:
 
         files = '<a href="'+self.url+'">link</a> '+name+'<br/><br/>'+time+'<br/><br/><br/>'
         files = files+ '<div style="border: 1px dashed grey;padding: 20px;">'+content+'</div><br><br/><br/>'
+        
+        replaysContent = ''
         for item in replays:
-            files = files + '—— <div>'+item+'</div><br/>'
-
-        os.mkdir('./file/'+time[0:10]+'-'+time[17:19])
-        f = open('./file/'+time[0:10]+'-'+time[17:19]+'/index.html', mode='wb')
-        f.write(files.encode('utf-8'))
-        f.close()
+            replaysContent = replaysContent + '—— <div>'+item+'</div><br/>'
 
 
-getFile('url').getHtml()
+        self.name = name
+        self.content = files
+        self.replaysContent = replaysContent
+        self.time = time
+        
+        # self.writeDB()
+        # os.mkdir('./file/'+time[0:10]+'-'+time[17:19])
+        # f = open('./file/'+time[0:10]+'-'+time[17:19]+'/index.html', mode='wb')
+        # f.write(files.encode('utf-8'))
+        # f.close()
+
+
+
+    def connetDB(self):
+        self.mydb = mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            passwd="123456",
+            database="astrology"
+        )
+        return self
+
+    def createDB(self):
+        cursor = self.mydb.cursor()
+        cursor.execute("CREATE DATABASE astrology")
+
+
+    def createTable(self):
+        cursor = self.mydb.cursor()
+        cursor.execute("CREATE TABLE articles (name VARCHAR(255), content LONGTEXT, back lONGTEXT, time VARCHAR(255))")
+        return self
+
+    def writeDB(self):
+        cursor = self.mydb.cursor()
+        sql = "INSERT INTO articles (name, content, back, time) VALUES (%s, %s, %s, %s)"
+        val = (self.name, self.content, self.replaysContent, self.time)
+        cursor.execute(sql, val)
+        self.mydb.commit()
+
+
+getFile('https://www.douban.com/group/topic/305852920/?_i=7676565cYhIypq').getHtml().connetDB().writeDB()
+
+# .getHtml().connetDB().writeDB()
+
+# getFile('url').connetDB().createTable()
 
 # getHtml().getContent()
